42d08d10b29e5fd2e11d183a860958d708f41e0e
David Turner <novalis@openplans.org>
2008-01-12 00:50:14 +0000

    read subscribers from a file
    git-svn-id: https://svn.socialplanning.org/svn/Cabochon/trunk@12611 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- cabochon/config/environment.py
+++ cabochon/config/environment.py
@@ -5,6 +5,27 @@ import cabochon.lib.app_globals as app_globals
 
 from cabochon.config.routing import make_map
 
+from ConfigParser import ConfigParser
+
+class ConfigError(Exception): pass
+
+from cabochon.models import *
+
+def subscribe_by_name(event, url):
+    """Subscribe a given URL to the event with the given name."""
+    try:
+        event_type = EventType.selectBy(name=event)[0]
+    except IndexError:
+        event_type = EventType(name=event)
+
+    subscriber = Subscriber.selectBy(event_type=event_type, url=url)
+    try:
+        subscriber = subscriber[0]
+        subscriber.set(url=url)
+    except IndexError:
+        subscriber = Subscriber(event_type=event_type, method="POST")
+
+
 def load_environment(global_conf={}, app_conf={}):
 
     # Setup our paths
@@ -28,3 +49,11 @@ def load_environment(global_conf={}, app_conf={}):
     
     # Add your own template options config options here, note that all
     # config options will override any Pylons config options
+
+
+    # initialize list of subscribers
+    sub_conf = ConfigParser()
+    sub_conf.read(config['subscriber_list_filename'])
+    for event, subscriber in sub_conf.items('subscribers'):
+        event, subscriber
+        subscribe_by_name (subscriber, event)
