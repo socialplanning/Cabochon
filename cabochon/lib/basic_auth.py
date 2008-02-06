0529f45302188083b238160d018d1d30051de09f
David Turner <novalis@openplans.org>
2008-02-06 16:51:22 +0000

    wow, that was buggy
    git-svn-id: https://svn.socialplanning.org/svn/Cabochon/trunk@13175 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- cabochon/lib/basic_auth.py
+++ cabochon/lib/basic_auth.py
@@ -8,24 +8,21 @@ class BasicAuthMiddleware:
         assert 'topp_admin_info_filename' in config
             
         admin_file = config['topp_admin_info_filename']
-        self.username, self.password = file(admin_file).read().strip().split(":")
-        if len(self.admin_info) != 2:
-            raise Exception("Bad format in administrator info file")
-        
-        self.username
+        self.username, self.password = file(admin_file).read().strip().split(":")        
+
         
     def __call__(self, environ, start_response):
 
         if not 'HTTP_AUTHORIZATION' in environ:
             head = [('WWW-Authenticate', 'Basic realm="cabochon"')]
-            raise HTTPUnauthorized(headers=head)
+            raise httpexceptions.HTTPUnauthorized(headers=head)
             
         basic, encoded = environ['HTTP_AUTHORIZATION'].split(" ")
         if basic != "Basic": return False
         username, password = encoded.decode("base64").split(":")
-        if not username == self.username and password == self.password:
+        if not (username == self.username and password == self.password):
             head = [('WWW-Authenticate', 'Basic realm="cabochon"')]
-            raise HTTPUnauthorized(headers=head)
+            raise httpexceptions.HTTPUnauthorized(headers=head)
 
         
         environ['REMOTE_USER'] = username
