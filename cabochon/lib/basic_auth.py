dc16410310eb61b57fd33bebbfe854329b03ab11
David Turner <novalis@openplans.org>
2008-05-20 19:53:48 +0000

    paste wigs out if you fail to follow wsgi
    git-svn-id: https://svn.socialplanning.org/svn/Cabochon/trunk@17116 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- cabochon/lib/basic_auth.py
+++ cabochon/lib/basic_auth.py
@@ -7,23 +7,26 @@ class BasicAuthMiddleware:
         admin_file = config['topp_admin_info_filename']
         self.username, self.password = file(admin_file).read().strip().split(":")
 
-        
+    def _fail(self, start_response):
+        head = [('WWW-Authenticate', 'Basic realm="cabochon"')]
+        start_response("401 Unauthorized", head)
+        return ["Bad or no authentication."]
+
     def __call__(self, environ, start_response):
         if 'REMOTE_USER' in environ:
             return self.app(environ, start_response)
 
         if not 'HTTP_AUTHORIZATION' in environ:
-            head = [('WWW-Authenticate', 'Basic realm="cabochon"')]
-            start_response("401 Unauthorized", head)
-            return []
-            
+            return self._fail(start_response)
+
         basic, encoded = environ['HTTP_AUTHORIZATION'].split(" ")
-        if basic != "Basic": return False
+
+        if basic != "Basic": 
+            return self._fail(start_response)
+  
         username, password = encoded.decode("base64").split(":", 1)
         if not (username == self.username and password == self.password):
-            head = [('WWW-Authenticate', 'Basic realm="cabochon"')]
-            start_response("401 Unauthorized", head)
-            return []
+            return self._fail(start_response)
 
         environ = environ.copy()
         environ['REMOTE_USER'] = username
