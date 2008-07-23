5de1adb384226b8362aa6d85974bb6d5b0453f6a
David Turner <novalis@openplans.org>
2008-07-23 18:49:51 +0000

    added a version number
    git-svn-id: https://svn.socialplanning.org/svn/Cabochon/trunk@19396 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Cabochon',
-    version="",
+    version="0.2",
     #description="",
     author="David Turner",
     author_email="novalis@openplans.org",
